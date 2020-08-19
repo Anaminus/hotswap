96e7eb9e8c2ca6f45578a4386669df69cc7fa9db
Anaminus <anaminus@protonmail.com>
2020-08-19 10:11:54 -0500

    Fix directory target.

--- build.lua
+++ build.lua
@@ -4,12 +4,9 @@ print("Building", targetPath)
 local plugin = DataModel.new()
 
 local function addDirectory(dir, target)
-	local folder = Instance.new("Folder")
-	folder.Name = target
-	folder.Parent = plugin
 	for _, f in ipairs(os.dir(dir)) do
 		local script = file.read(os.join(dir, f.Name), "modulescript.lua")
-		script.Parent = folder
+		script.Parent = target
 	end
 end
 
@@ -19,11 +16,12 @@ file.read("src/Const.lua").Parent = plugin
 local assets = file.read("src/Assets.lua")
 assets.Parent = plugin
 file.read("assets/data.lua").Parent = assets
-file.read("src/Lion.lua").Parent = plugin
+local lion = file.read("src/Lion.lua")
+lion.Parent = plugin
 file.read("src/Path.lua").Parent = plugin
 file.read("src/Tooltip.lua").Parent = plugin
 file.read("src/Widget.lua").Parent = plugin
 
-addDirectory("l10n", "Lion")
+addDirectory("l10n", lion)
 
 file.write(targetPath, plugin, "rbxm")
