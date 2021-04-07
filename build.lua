9263b861466245882ad7baa13ad001c834ddd8d1
Anaminus <anaminus@protonmail.com>
2021-04-07 07:43:15 -0500

    Update build script to use latest rbxmk API.

--- build.lua
+++ build.lua
@@ -4,24 +4,26 @@ print("Building", targetPath)
 local plugin = DataModel.new()
 
 local function addDirectory(dir, target)
-	for _, f in ipairs(os.dir(dir)) do
-		local script = file.read(os.join(dir, f.Name), "modulescript.lua")
-		script.Parent = target
+	for _, f in ipairs(fs.dir(dir)) do
+		if not f.IsDir then
+			local script = fs.read(os.join(dir, f.Name), "modulescript.lua")
+			script.Parent = target
+		end
 	end
 end
 
-file.read("src/Main.lua", "script.lua").Parent = plugin
-file.read("src/HotSwap.lua").Parent = plugin
-file.read("src/Const.lua").Parent = plugin
-local assets = file.read("src/Assets.lua")
+fs.read("src/Main.lua", "script.lua").Parent = plugin
+fs.read("src/HotSwap.lua").Parent = plugin
+fs.read("src/Const.lua").Parent = plugin
+local assets = fs.read("src/Assets.lua")
 assets.Parent = plugin
-file.read("assets/data.lua").Parent = assets
-local lion = file.read("src/Lion.lua")
+fs.read("assets/data.lua").Parent = assets
+local lion = fs.read("src/Lion.lua")
 lion.Parent = plugin
-file.read("src/Path.lua").Parent = plugin
-file.read("src/Tooltip.lua").Parent = plugin
-file.read("src/Widget.lua").Parent = plugin
+fs.read("src/Path.lua").Parent = plugin
+fs.read("src/Tooltip.lua").Parent = plugin
+fs.read("src/Widget.lua").Parent = plugin
 
 addDirectory("l10n", lion)
 
-file.write(targetPath, plugin, "rbxm")
+fs.write(targetPath, plugin, "rbxm")
